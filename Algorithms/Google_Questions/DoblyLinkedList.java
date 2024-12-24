72f7c774b69e48fb884fc17ef9c68420e51cec95
infx012685 <dipali.gangawane@infinx.com>
2024-12-24 12:22:43 +0530

    new folders created

---
+++ Algorithms/Google_Questions/DoblyLinkedList.java
@@ -0,0 +1,9 @@
+public class DoblyLinkedList {
+
+    //Given a double linked list, and a set of node pointers, find how many connected components in the set.
+    //For example, if a double linked list is: 1-2-3-4-5 and set contains pointers to [1 , 2, 5], this set contains 2 componments, one is [ 1 2], second is [5] since [5] is disconnected. The set [1 2 3 4 5] contains only one componment since everything is connected.
+
+    public static void main(String[] args) {
+        
+    }
+}
