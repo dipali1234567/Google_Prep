7373129ad6f5ba91879750815b63990504a589af
infx012685 <dipali.gangawane@infinx.com>
2024-12-24 14:05:01 +0530

    Added new folders to write code

---
+++ Algorithms/DisjointSet/Disjoint.java
@@ -0,0 +1,13 @@
+public class Disjoint {
+
+    // What is a disjoint set?
+    // Two components taken
+    // There is nothing com mon between them called disjoint
+    // They are separate
+    // We can perform union and find operations on them
+    // Find - Find out the elements in which set.
+    // Union - Merge two sets
+
+
+    
+    }
--- DataStructures/linkedlist.java
+++ DataStructures/linkedlist.java
@@ -1,17 +1,5 @@
 public class linkedlist {
 
-    
-
-    public static void main(String[] args) {
-        Node head = new Node(10);
-        Node second = new Node(20);
-        Node third = new Node(30);
-
-        head.next = second;
-        second.next = third;
-
-        System.out.println(head.data);
-    }
 }
 
 
